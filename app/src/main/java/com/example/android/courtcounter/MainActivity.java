9cfa6564c3b3b4e3c19955b25d1fe96851992f12
aslanchex <aslan.kashiev@mail.ru>
2020-04-20 14:38:33 +0300

    Изменена стилизация приложения

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -14,6 +14,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private int scoreTeamA = 0;
     private int scoreTeamB = 0;
+    //boolean timerIsStarted = false; //для таймера
+    //int time = 600; // для таймера
 
     /*
      * Метод onCreate выполняется во время запуска приложения
@@ -28,39 +30,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /*
-     * Метод устанавливает 1 очко на табло
+     * Метод устанавливает 1 очко на табло, при условии, что таймер запущен
      */
     public void addOnePoint(View view) {
-        if (view.getId() == R.id.add_one_score_teamA) {
+        if (view.getId() == R.id.add_one_score_teamA/* && timerIsStarted*/) {
             scoreTeamA += 1;
             displayForTeamA(scoreTeamA);
-        } else {
+        } else if (view.getId() == R.id.add_one_score_teamB/* && timerIsStarted*/) {
             scoreTeamB += 1;
             displayForTeamB(scoreTeamB);
         }
     }
 
     /*
-     * Метод устанавливает 2 очка на табло
+     * Метод устанавливает 2 очка на табло, при условии, что таймер запущен
      */
     public void addTwoPoint(View view) {
-        if (view.getId() == R.id.add_two_score_teamA) {
+        if (view.getId() == R.id.add_two_score_teamA/* && timerIsStarted*/) {
             scoreTeamA += 2;
             displayForTeamA(scoreTeamA);
-        } else {
+        } else if (view.getId() == R.id.add_two_score_teamB/* && timerIsStarted*/) {
             scoreTeamB += 2;
             displayForTeamB(scoreTeamB);
         }
     }
 
     /*
-     * Метод устанавливает 3 очка на табло
+     * Метод устанавливает 3 очка на табло, при условии, что таймер запущен
      */
     public void addThreePoint(View view) {
-        if (view.getId() == R.id.add_three_score_teamA) {
+        if (view.getId() == R.id.add_three_score_teamA/* && timerIsStarted */) {
             scoreTeamA += 3;
             displayForTeamA(scoreTeamA);
-        } else {
+        } else if (view.getId() == R.id.add_three_score_teamB/* && timerIsStarted*/) {
             scoreTeamB += 3;
             displayForTeamB(scoreTeamB);
         }
@@ -81,10 +83,36 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.score_viewB);
         scoreView.setText(String.valueOf(score));
     }
-
+    /*
+    * Метод сбрасывает счет игры и время
+     */
     public void resetCounter(View view) {
         scoreTeamA = scoreTeamB = 0;
         displayForTeamA(0);
         displayForTeamB(0);
+        //time = 600; // для таймера
+        //timerIsStarted = false; // для таймера
     }
+
+    /*
+    * Метод устанавливает таймер
+     */
+//    public void timer(View view) {
+//
+//        try {
+//            while (time != 0) {
+//                //Thread.sleep(1000);
+//                time--;
+//                displayTimer(time);
+//                timerIsStarted = true;
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+//    private void displayTimer(int time) {
+//        TextView timer = (TextView) findViewById(R.id.timer);
+//        timer.setText(String.valueOf(time));
+//    }
 }
