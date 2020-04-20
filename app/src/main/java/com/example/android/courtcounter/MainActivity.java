c76c729c4c54206560873f020c9708344b562f91
aslanchex <aslan.kashiev@mail.ru>
2020-04-20 13:43:41 +0300

    Изменена иконка приложения. Добавлены стили главногго меню.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -84,5 +84,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void resetCounter(View view) {
         scoreTeamA = scoreTeamB = 0;
+        displayForTeamA(0);
+        displayForTeamB(0);
     }
 }
